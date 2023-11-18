407c19b7961b11901981f60b753c9e21377bdbfe
Johnny Quid <john@quid.io>
2023-11-19 00:15:07 +0200

    finally got cargo test working

--- contracts/QD/tests/harness.rs
+++ contracts/QD/tests/harness.rs
@@ -1,32 +1,61 @@
 use fuels::{
-    prelude::{abigen, Config, Contract, LoadConfiguration, StorageConfiguration, TxParameters},
-    types::{errors::Result, Bits256},
+    accounts::wallet::WalletUnlocked,
+    types::ContractId,
+    prelude::{
+        abigen, Contract, 
+        LoadConfiguration, 
+        StorageConfiguration, 
+        TxParameters
+    },
     programs::contract::SettableContract,
     test_helpers::launch_provider_and_get_wallet,
-    accounts::wallet::WalletUnlocked,
 };
 
-
-#[tokio::test]
-async fn contract_call() -> Result<()> {
-    use fuels::prelude::*;
-
-    abigen!(Contract(
-        name = "Quid",
+// The following macro will automatically generate some structs for you, to easily interact with contracts and scripts.
+abigen!(
+    Contract(
+        name = "QD",
         abi = "contracts/QD/out/debug/QD-abi.json"
-    ));
+    ),
+);
 
+// File path constants
+const STORAGE_CONFIGURATION_PATH: &str =
+    "out/debug/QD-storage_slots.json";
+const CONTRACT_BIN_PATH: &str = "out/debug/QD.bin";
+
+
+// This function will setup the test environment for you. It will return a tuple containing the contract instance and the script instance.
+pub async fn setup() -> (
+    QD<WalletUnlocked>
+) {
+    // The `launch_provider_and_get_wallet` function will launch a local provider and create a wallet for you.
     let wallet = launch_provider_and_get_wallet().await;
 
-    let contract_id = Contract::load_from(
-        "contracts/QD/out/debug/QD.bin",
+    // The following code will load the storage configuration (default storage values) from the contract and create a configuration object.
+    // let storage_configuration =
+    //     StorageConfiguration::load_from(STORAGE_CONFIGURATION_PATH).unwrap();
+    // let configuration =
+    //     LoadConfiguration::default().with_storage_configuration(storage_configuration);
+
+    // The following code will deploy the contract and store the returned ContractId in the `id` variable.
+    let id = Contract::load_from(
+        CONTRACT_BIN_PATH, 
         LoadConfiguration::default(),
-    )?
+    ).unwrap()
     .deploy(&wallet, TxParameters::default())
-    .await;
+    .await
+    .unwrap();
 
-    // ANCHOR: contract_call_cost_estimation
-    let contract_instance = Quid::new(contract_id, wallet);
+    // Creates a contract instance and a script instance. Which allow for easy interaction with the contract and script.
+    let contract_instance = QD::new(id, wallet.clone());
 
-    Ok(())
+    (contract_instance)
+}
+
+#[tokio::test]
+async fn test_script() {
+    // Call the setup function to deploy the contract and create the contract instance
+    let contract_instance = setup().await;
+    // assert_eq!(result, 0);
 }
