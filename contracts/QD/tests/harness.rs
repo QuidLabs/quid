7a36c16390dcba36dcc00a1510d7395adf2d13f4
Johnny Quid <john@quid.io>
2023-11-18 23:42:05 +0200

    StorageConfiguration::load_from function not found

--- contracts/QD/tests/harness.rs
+++ contracts/QD/tests/harness.rs
@@ -1,61 +1,60 @@
-use fuels::{prelude::*, types::ContractId};
- 
-// Load abi from json
-abigen!(Contract, "./out/debug/QD-abi.json");
- 
-async fn get_contract_instance() -> (Contract, ContractId) {
-	// Launch a local network and deploy the contract
-	let mut wallets = launch_custom_provider_and_get_wallets(
-		WalletsConfig::new(
-			Some(1),			 /* Single wallet */
-			Some(1),			 /* Single coin (UTXO) */
-			Some(1_000_000_000), /* Amount per coin */
-		),
-		None,
-	)
-	.await;
-	let wallet = wallets.pop().unwrap();
- 
-	let id = Contract::load_from(
-		"./out/debug/QD.bin",
-		LoadConfiguration::default().set_storage_configuration(
-			StorageConfiguration::load_from(
-				"./out/debug/QD-storage_slots.json",
-			)
-			.unwrap(),
-		),
-	)
-	.unwrap()
-	.deploy(&wallet, TxParameters::default())
-	.await
-	.unwrap();
- 
-	let instance = Contract::new(id.to_string(), wallet);
- 
-	(instance, id.into())
+use fuels::{
+    accounts::wallet::WalletUnlocked,
+    types::ContractId,
+    prelude::{
+        abigen, Contract, 
+        LoadConfiguration, 
+        StorageConfiguration, 
+        TxParameters
+    },
+    programs::contract::SettableContract,
+    test_helpers::launch_provider_and_get_wallet,
+};
+
+// The following macro will automatically generate some structs for you, to easily interact with contracts and scripts.
+abigen!(
+    Contract(
+        name = "QD",
+        abi = "./contracts/QD/out/debug/QD-abi.json"
+    ),
+);
+
+// File path constants
+const STORAGE_CONFIGURATION_PATH: &str =
+    ".contracts/QD/out/debug/QD-storage_slots.json";
+const CONTRACT_BIN_PATH: &str = "./contracts/QD/out/debug/QD.bin";
+
+
+// This function will setup the test environment for you. It will return a tuple containing the contract instance and the script instance.
+pub async fn setup() -> (
+    QD<WalletUnlocked>,
+    ContractId
+) {
+    // The `launch_provider_and_get_wallet` function will launch a local provider and create a wallet for you.
+    let wallet = launch_provider_and_get_wallet().await;
+
+    // The following code will load the storage configuration (default storage values) from the contract and create a configuration object.
+    let storage_configuration =
+        StorageConfiguration::load_from(STORAGE_CONFIGURATION_PATH).unwrap();
+    let configuration =
+        LoadConfiguration::default().with_storage_configuration(storage_configuration);
+
+    // The following code will deploy the contract and store the returned ContractId in the `id` variable.
+    let id = Contract::load_from(CONTRACT_BIN_PATH, configuration)
+        .unwrap()
+        .deploy(&wallet, TxParameters::default())
+        .await
+        .unwrap();
+
+    // Creates a contract instance and a script instance. Which allow for easy interaction with the contract and script.
+    let contract_instance = QD::new(id, wallet.clone());
+
+    (contract_instance, id.into())
 }
- 
+
 #[tokio::test]
-async fn initialize_and_increment() {
-	let (contract_instance, _id) = get_contract_instance().await;
-	// Now you have an instance of your contract you can use to test each function
- 
-	// let result = contract_instance
-	// 	.methods()
-	// 	.initialize_counter(42)
-	// 	.call()
-	// 	.await
-	// 	.unwrap();
- 
-	// assert_eq!(42, result.value);
- 
-	// // Call `increment_counter()` method in our deployed contract.
-	// let result = contract_instance
-	// 	.methods()
-	// 	.increment_counter(10)
-	// 	.call()
-	// 	.await
-	// 	.unwrap();
- 
-	// assert_eq!(52, result.value);
+async fn test_script() {
+    // Call the setup function to deploy the contract and create the contract instance
+    let (contract_instance, _id) = setup().await;
+    // assert_eq!(result, 0);
 }
